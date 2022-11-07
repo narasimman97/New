b2e451953a1d87114c31d2fd1e42b9a71b5cdf23
narasimman97 <narasimma97gmail.com>
2022-11-07 11:00:24 +0530

    Add three methods

---
+++ src/test/java/com/Practice/Sample.java
@@ -0,0 +1,16 @@
+package com.Practice;
+
+public class Sample {
+	public void india() {
+	System.out.println("india");
+
+}
+	public void australia() {
+	System.out.println("aus");
+
+}
+	public void japan() {
+	System.out.println("japan");
+
+}
+}
